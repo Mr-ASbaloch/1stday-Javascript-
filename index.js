ecfd68434e33f86e9a2207980d2719cb2a9670fa
abdul <abdulsaeed1356@gmail.com>
2023-02-16 11:15:05 +0500

    added

--- index.js
+++ index.js
@@ -1,7 +1,87 @@
-// let name="saeed";
-// console.log(name);
-var a;
-a=5;
-console.log(a);
-// alert(a);
-// alert(2+54);
+// variablels
+// use of var 
+var digit1=23;
+var digit2=45;
+var sum=digit1+digit2;
+console.log(sum)
+
+// let 
+let x;
+x="saeed";
+console.log(x);
+let y;
+console.log(y);  //undefined 
+
+// const
+const digit=32;
+console.log(digit);
+
+// var redeclare
+var k="saeed";
+var k=34;
+console.log(k)
+
+//  let redeclare cant,t be same variable name show error
+let s="ali";
+let z=40;
+console.log(z) 
+
+
+// blockscope
+var j=5;
+console.log(j)
+{
+    var j=2;
+    console.log(j);
+}
+
+let m=5;
+console.log(m)
+{
+    let l=10;
+    console.log(l);
+}
+const pi=3.14;
+ mega= 2.12;
+console.log(mega);
+
+// operators
+
+// arithematic operator 
+let int1=7;
+let int2=5;
+let int3=8;
+let add=int1+int2; //sum -operator
+console.log(add);
+let sub=int1-int2; //subtract -operator
+console.log(sub);
+let mul=int1*int2; //multiplication -operator
+console.log(mul);
+let divid=int1/int2; //dividision -operator
+console.log(divid); 
+let mod=int1%int2; //modolus -operator
+console.log(mod);
+
+let h=6;
+//h++;
+let n=++h;   //increment operator
+console.log(n);
+let p=6;
+//p--;
+let t=--p;   //decrement operator
+console.log(t);
+
+let a=4; //exponentiation operator
+let b=a**2;
+console.log(b)
+
+
+
+
+
+
+
+
+
+
+
