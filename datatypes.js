35c0c5f7c56de130c0567756d6e58a8d32fbd240
abdul <abdulsaeed1356@gmail.com>
2023-02-16 12:43:25 +0500

    addded

---
+++ datatypes.js
@@ -0,0 +1,51 @@
+//data types
+
+
+//number
+let length=13;
+let age=20;
+console.log(length,age);
+
+//string
+let name="saeed";
+let cost="baloch";
+console.log(name,cost);
+
+//boolean
+let x=true;
+let y=false;
+console.log(y);
+
+//string and num 
+
+let age1=24;
+let num="mr"
+let x1=age1+num;
+console.log(x1);
+
+let y1="abc"+ 16+ 4;
+console.log(y1);
+
+let j=4;
+let k=5
+ j==k;
+ console.log(j==k) //return false
+
+//  undefined
+let y2; 
+console.log(y2);
+
+
+//empty
+let z="";
+console.log(z);
+
+//type of 
+let age3=42;
+console.log(typeof(age3))
+
+
+
+
+
+
